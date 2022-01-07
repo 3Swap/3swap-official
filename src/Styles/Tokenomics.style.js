634c80e22bed0bdef3b507e61d304d3cd76add4c
damii dennison <dwanedehydra@gmail.com>
2022-01-07 03:56:47 +0100

    tokenomics section half done

--- src/Styles/Tokenomics.style.js
+++ src/Styles/Tokenomics.style.js
@@ -4,13 +4,31 @@ import styled from "styled-components";
 const TokenomicsWrapper = styled.div`
 display: flex;
 .tokenomics-container{
+    padding:40px;
     .tokenomics{
         display:flex;
         flex-direction:column;
+        margin-top: 50px;
+        p{
+            width:290px
+
+        }
     }
     .details{
-        display: flex;
-        flex-direction: column;
+        margin-top:20px;
+        .container{
+            display:flex;
+            align-items: center;
+            margin-top: 5px;
+            svg{
+                width:20px;
+                height:20px;
+            }
+            h3{
+                margin-left:10px;
+                font-size: 16px;
+            }
+        }
     }
 }
 `
