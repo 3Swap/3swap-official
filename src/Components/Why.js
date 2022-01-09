a967674e6c5220665134848c642acfc6782fc64f
Anuforo Okechukwu Deede <okeydeede@gmail.com>
2022-01-09 14:38:43 +0100

    modified why section

--- src/Components/Why.js
+++ src/Components/Why.js
@@ -1,63 +1,64 @@
-import { CardArrowIcon, FlexibilityIcon, LowCostIcon, RewardIcon } from "../Assets/Utility";
-import { Primary } from "../Styles/Buttons.stye";
-import WhyWrapper from "../Styles/Why.style";
+import {
+  CardArrowIcon,
+  FlexibilityIcon,
+  LowCostIcon,
+  RewardIcon,
+} from '../Assets/Utility';
+import { Primary } from '../Styles/Buttons.stye';
+import WhyWrapper from '../Styles/Why.style';
 
 function Why() {
-    return (
-			<WhyWrapper>
-				<div className="why-content">
-					<h1>Why invest in 3swap</h1>
-					<p>
-						We Are Introducing A New Paradigm In The Decentralized Finance
-						Space.
-					</p>
-					<Primary>
-						<a href="#">Read whitePaper</a>
-					</Primary>
-				</div>
-				<div className="why-cards">
-					<div className="card">
-						<div className="heading">
-							<div>
-								<img src={FlexibilityIcon} alt="" />
-							</div>
-							<h3>Flexibility</h3>
-						</div>
-						<p>Swap Two Tokens For One Across Various EVM-Compatible Chains.</p>
-						<img src={CardArrowIcon} alt="" className="card-arrow" />
-					</div>
-					<div className="card">
-						<div className="heading">
-							<div>
-								<img src={LowCostIcon} alt="" />
-							</div>
-							<h3>Low cost</h3>
-						</div>
-						<p>
-							3swap Aims To Be Financially Conducive And/Or Appealing By
-							Allowing You To Pay Less For Swaps Executed.
-						</p>
-						<img src={CardArrowIcon} alt="" className="card-arrow" />
-					</div>
-					<div className="card">
-						<div className="heading">
-							<div>
-								<img src={RewardIcon} alt="" />
-							</div>
-							<h3>More rewards</h3>
-						</div>
-						<p>
-							3swap Aims To Offer More Value For The Provision Of Liquidity
-							Across Three Tokens.
-						</p>
-						<img src={CardArrowIcon} alt="" className="card-arrow" />
-					</div>
-				</div>
-			</WhyWrapper>
-		);
-
+  return (
+    <WhyWrapper>
+      <div className="why-content">
+        <h1>Why invest in 3swap</h1>
+        <p>
+          We Are Introducing A New Paradigm In The Decentralized Finance Space.
+        </p>
+        <Primary>
+          <a href="/">Read whitePaper</a>
+        </Primary>
+      </div>
+      <div className="why-cards">
+        <div className="card">
+          <div className="heading">
+            <div>
+              <img src={FlexibilityIcon} alt="" />
+            </div>
+            <h3>Flexibility</h3>
+          </div>
+          <p>Swap Two Tokens For One Across Various EVM-Compatible Chains.</p>
+          <img src={CardArrowIcon} alt="" className="card-arrow" />
+        </div>
+        <div className="card">
+          <div className="heading">
+            <div>
+              <img src={LowCostIcon} alt="" />
+            </div>
+            <h3>Low cost</h3>
+          </div>
+          <p>
+            3swap Aims To Be Financially Conducive And/Or Appealing By Allowing
+            You To Pay Less For Swaps Executed.
+          </p>
+          <img src={CardArrowIcon} alt="" className="card-arrow" />
+        </div>
+        <div className="card">
+          <div className="heading">
+            <div>
+              <img src={RewardIcon} alt="" />
+            </div>
+            <h3>More rewards</h3>
+          </div>
+          <p>
+            3swap Aims To Offer More Value For The Provision Of Liquidity Across
+            Three Tokens.
+          </p>
+          <img src={CardArrowIcon} alt="" className="card-arrow" />
+        </div>
+      </div>
+    </WhyWrapper>
+  );
 }
-
-
 
 export default Why;
--- src/Styles/RoadMap.style.js
+++ src/Styles/RoadMap.style.js
@@ -1,30 +1,28 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const RoadMapWrapper = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-    margin-top: 30px;
-	svg {
-		width: 85%;
-	}
-	.roadmap-content {
-		display: flex;
-		margin-top: 20px;
-		width: 85%;
-		.content-container {
-			margin-left: 10px;
-			p {
-				width: 200px;
-				font-size: 16px;
-			}
-			:nth-child(1) {
-				margin-left: 20px;
-			}
-		}
-	}
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 30px;
+  svg {
+    width: 85%;
+  }
+  .roadmap-content {
+    display: flex;
+    margin-top: 20px;
+    width: 85%;
+    .content-container {
+      margin-left: 10px;
+      p {
+        width: 200px;
+        font-size: 16px;
+      }
+      :nth-child(1) {
+        margin-left: 20px;
+      }
+    }
+  }
 `;
-
-
 
 export default RoadMapWrapper;
